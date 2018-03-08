544ee507375bcd9f3ab25a946f21f048eb5c420d
spy <sp831031@gmail.com>
2018-03-08 14:19:54 +0800

    新增後台create action,初步完成新增會員功能

--- app/controllers/cashier/members_controller.rb
+++ app/controllers/cashier/members_controller.rb
@@ -6,4 +6,23 @@ class Cashier::MembersController < ApplicationController
   def new
     @member = Member.new
   end
+
+  def create
+    @member = Member.new(member_params)
+    if @member.save!
+      flash[:notice] = "成功新增會員"
+      redirect_to cashier_root_path
+    else
+      flash[:alert] = @member.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:name,:gender,:birthday,
+                                  :phone,:email,:skin,:hair,
+                                  :avatar,:remark)
+  end
 end
