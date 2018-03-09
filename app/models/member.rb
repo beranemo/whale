4a2bafcfb93938fe137356415164300ec37b2ff5
beranemo <beranemo@gmail.com>
2018-03-09 19:31:42 +0800

    更新 model 異動

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,20 @@
+# == Schema Information
+#
+# Table name: members
+#
+#  id         :integer          not null, primary key
+#  name       :string           default(""), not null
+#  birthday   :date             not null
+#  gender     :string           default(""), not null
+#  phone      :string           default(""), not null
+#  skin       :string
+#  hair       :string
+#  avatar     :string
+#  remark     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  email      :string           default(""), not null
+#
+
 class Member < ApplicationRecord
 end
--- test/models/guest_test.rb
+++ test/models/guest_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: guests
+#
+#  id          :integer          not null, primary key
+#  age         :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  country_id  :integer
+#  sex_id      :integer
+#  info_way_id :integer
+#
+
 require 'test_helper'
 
 class GuestTest < ActiveSupport::TestCase
