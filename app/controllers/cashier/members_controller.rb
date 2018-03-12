425d7e74c0911635d965c26c762dbe972a65b38c
spy <sp831031@gmail.com>
2018-03-12 10:36:11 +0800

    移除search action中的清除session動作，更新手機搜尋的前端顯示

--- app/controllers/cashier/members_controller.rb
+++ app/controllers/cashier/members_controller.rb
@@ -21,9 +21,7 @@ class Cashier::MembersController < ApplicationController
   end
 
   def search
-    @order = Order.find_by(id: session[:order_id])
-    @order.destroy
-    session[:order_id] = nil
+    
   end
 
  
@@ -33,11 +31,11 @@ class Cashier::MembersController < ApplicationController
     key_word = ''
     if params[:phone] != nil
       key_word = params[:phone]
-      @member = Member.where("phone like ?", "%"+key_word+"%")
-      puts @member
+      @members = Member.where("phone like ?", "%"+key_word+"%")
+      puts @members
       # 當@member 回傳有多筆資料時，前端介面資料承接方式要改一下
       # FIXME
-      render :json => {:id => @member.id, :name => @member.name, :phone =>@member.phone, :gender => @member.gender, :email => @member.email, :birthday => @member.birthday}
+      render :json => @members
     elsif params[:email] != nil
       key_word = params[:email]
       # find_by 只會找出一筆資料，用 where 才能找出多筆，這裡還未改成 where 方便與 phone 的 where 辨異
