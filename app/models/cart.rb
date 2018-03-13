430601e9738bf1190fdf36a41daaf1312db792bc
spy <sp831031@gmail.com>
2018-03-13 13:34:41 +0800

    設定cart,cart_item和product之間的關聯

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,3 @@
 class Cart < ApplicationRecord
+  has_many :cart_items, dependent: :destroy  
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -21,6 +21,7 @@
 
 class Product < ApplicationRecord
   has_many :order_items
+  has_many :cart_items
   def self.update_by_file(file)
     attribute = Hash["類型" => "category" ,
                     "中文品名"=> "zh_name",
