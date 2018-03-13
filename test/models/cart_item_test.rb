a34e3bbf8bc3b3fd5b97c61e9311322268990a66
beranemo <beranemo@gmail.com>
2018-03-13 20:58:17 +0800

    調整tabbar配置

--- test/models/cart_item_test.rb
+++ test/models/cart_item_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: cart_items
+#
+#  id         :integer          not null, primary key
+#  cart_id    :integer
+#  product_id :integer          not null
+#  quantity   :integer          default(1)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class CartItemTest < ActiveSupport::TestCase
