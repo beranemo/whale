faa6381c4b053dfa2f89af190ef50f6c3934575e
spy <sp831031@gmail.com>
2018-03-13 17:18:05 +0800

    修正成立訂單功能

--- app/controllers/cashier/orders_controller.rb
+++ app/controllers/cashier/orders_controller.rb
@@ -1,29 +1,40 @@
 class Cashier::OrdersController < ApplicationController
 
   def new
-    @amount = 0
+    @order =Order.new(member_id: params[:id])
+    @order.amount = 0
     @products = Product.all 
     @cart_items = current_cart.cart_items.all
     if params[:id] != "-1"
       
       @member = Member.find(params[:id])
     else
-      @member = Member.new
+      @member = Member.new(id: -1)
     end
   end
 
   def create
-    if current_order.order_items.size ==0
+    if current_cart.cart_items.size ==0
       flash[:alert] = "訂單內容不能是空的"
       @products = Product.all 
-      @order_items = current_order.order_items.all
-      @member = current_order.member_id ? 
-                 Member.new : Member.find(current_order.member_id)
+      @cart_items = current_cart.cart_items.all
+      @member =  Member.find(params[:member_id])
       render :new
     else
-      session[:order_id] = nil
-      redirect_to new_cashier_orders_path(id: -1)
+      @order = current_user.orders.build(member_id: params[:member_id])
+      @order.amount =0
+      current_cart.cart_items.each do |item|
+        order_item = @order.order_items.build(product_id: item.product.id, price: item.product.price, quantity: item.quantity)
+        @order.amount += item.product.price * item.quantity
+        order_item.save!
+      end
+      @order.save!
+      session[:cart_id] = nil
+      redirect_to new_cashier_order_path(id: -1)
     end
     
   end
+
+
+  
 end
