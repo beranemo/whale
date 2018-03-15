2eb2bb0b805ace90bdfb8a19fc2a87d27d90bc1a
beranemo <beranemo@gmail.com>
2018-03-15 13:13:39 +0800

    更新model註釋

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: bulletins
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  content    :text
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Bulletin < ApplicationRecord
   belongs_to :user
 end
