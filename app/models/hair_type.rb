7bb7182edd0e8dbfaa27bb823c64933352fa62e9
beranemo <beranemo@gmail.com>
2018-03-15 17:41:16 +0800

    更新註釋

--- app/models/hair_type.rb
+++ app/models/hair_type.rb
@@ -5,7 +5,7 @@
 #  id          :integer          not null, primary key
 #  code        :string
 #  value       :string
-#  system_flag :string
+#  system_flag :string           default("N")
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
--- app/models/skin_type.rb
+++ app/models/skin_type.rb
@@ -2,10 +2,13 @@
 #
 # Table name: skin_types
 #
-#  id         :integer          not null, primary key
-#  skin_type  :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :integer          not null, primary key
+#  skin_type   :string
+#  code        :string
+#  value       :string
+#  system_flag :string           default("N")
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
 #
 
 class SkinType < ApplicationRecord
