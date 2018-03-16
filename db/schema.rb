7fbf6f3c696c3bfefccee3b184b29dbc4f4bab00
spy <sp831031@gmail.com>
2018-03-16 13:18:12 +0800

    將schema version紀錄更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20180315151515) do
+ActiveRecord::Schema.define(version: 20180315154003) do
 
   create_table "ages", force: :cascade do |t|
     t.string "age_type"
