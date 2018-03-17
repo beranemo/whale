15fb01b614ad4b424a9550e5e283a1945b8de12e
Yipeechen <yipeechen0418@gmail.com>
2018-03-17 17:46:59 +0800

    新增訂單頁面與路徑

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,7 @@ Rails.application.routes.draw do
       end
     end
 
-    resources :orders, only: [:index, :new, :create, :edit, :update]
+    resources :orders, only: [:index, :new, :create, :edit, :update , :show]
 
     resources :cart_items, only: [:destroy] do
       member do
