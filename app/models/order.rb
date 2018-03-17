a1cbb9a4b38575ddfa12afc5075058e19e9daaee
Yipeechen <yipeechen0418@gmail.com>
2018-03-17 23:11:57 +0800

    完成訂單內容頁面,尚未置入打折價 & 小記欄位

--- app/models/order.rb
+++ app/models/order.rb
@@ -19,4 +19,5 @@ class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
   belongs_to :user
   validates_presence_of :member_id, :payment_method, :address
+  has_many :order_products, through: :order_items, source: :product
 end
