6c71a658378f9872d3ac1eec6e874fd3fe4c2654
beranemo <beranemo@gmail.com>
2018-03-17 15:38:00 +0800

    串顧客申請會員秀成功訊息

--- config/routes.rb
+++ config/routes.rb
@@ -59,5 +59,5 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
 
 #  root "members#index"
-  resources :members, only: [:index, :new, :create]
+  resources :members, only: [:index, :new, :create, :show]
 end
