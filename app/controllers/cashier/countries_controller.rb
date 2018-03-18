953f5d9bcddcfe3ec71b9264e5cb9d13e75f8719
beranemo <beranemo@gmail.com>
2018-03-18 23:24:28 +0800

    完成國籍設定瀏覽、刪除、新增功能

--- app/controllers/cashier/countries_controller.rb
+++ app/controllers/cashier/countries_controller.rb
@@ -1,2 +1,50 @@
 class Cashier::CountriesController < ApplicationController
+  before_action :set_country, only: [:update, :destroy]
+
+  def index
+    @countries = Country.all
+    @country = Country.new
+  end
+  
+  def update
+    if @country.update(country_params)
+      redirect_to cashier_countries_path
+      flash[:notice] = "國家設定資料已更新"
+    else
+      @countries = Country.all
+      render :index
+    end
+  end
+  
+  def create
+    @country = Country.new(country_params)
+    
+    if @country.save
+      flash[:notice] = "國家設定資料已更新"
+      redirect_to cashier_countries_path
+    else
+      @countries = Country.all
+      render :index
+    end
+    
+  end
+  
+  def destroy   
+    if @country.destroy
+      flash[:notice] = "國家設定資料已更新"
+      redirect_to cashier_countries_path
+    else
+      @countries = Country.all
+      render :index
+    end
+  end
+  
+  private
+  def country_params
+    params.require(:country).permit(:country_type)
+  end
+  
+  def set_country
+    @country = Country.find(params[:id])
+  end  
 end
