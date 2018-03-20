929846d04eeb9ec21dca2cb5fdabc9de0cc95b0a
spy <sp831031@gmail.com>
2018-03-20 15:29:44 +0800

    在discounts table新增discount_price 欄位

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320072609) do
+ActiveRecord::Schema.define(version: 20180320072824) do
 
   create_table "ages", force: :cascade do |t|
     t.string "age_type"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20180320072609) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "discount_method_id"
+    t.integer "discount_price"
   end
 
   create_table "guest_types", force: :cascade do |t|
