584e8787da837ef2610cde2e82fe39210064a95c
spy <sp831031@gmail.com>
2018-03-22 15:12:16 +0800

    修改成立訂單頁面1購買內容所顯示的資料,新增設定商品優惠價功能

--- app/controllers/cashier/cart_items_controller.rb
+++ app/controllers/cashier/cart_items_controller.rb
@@ -2,14 +2,16 @@ class Cashier::CartItemsController < ApplicationController
   
   def plus_quantity
     @cart_item = current_cart.cart_items.find_by(product_id: params[:id])
-    @o_price = @cart_item.calculate.round
-    puts @o_price
+    @l_price = @cart_item.calculate.round#上次金額
     @cart_item.quantity+=1
+    @o_price = @cart_item.origin_calculate.round#小計金額
     @cart_item.save!
-    @price = @cart_item.calculate.round
-    puts @price
-    render :json => {:id =>params[:id] , :quantity => @cart_item.quantity, :price =>@price -@o_price,
-                    :o_price => @price}
+    @d_price = @cart_item.calculate.round
+    puts @o_price
+    puts @l_price
+    puts @d_price
+    render :json => {:id =>params[:id] , :quantity => @cart_item.quantity, :price => @l_price,
+                    :v_price => @o_price - @d_price, :o_price => @o_price, :d_price => @d_price}
   end
 
 
@@ -17,15 +19,16 @@ class Cashier::CartItemsController < ApplicationController
     @cart_item = current_cart.cart_items.find_by(product_id: params[:id])
     
     if @cart_item.quantity>0
-      @o_price = @cart_item.calculate.round
+      @l_price = @cart_item.calculate.round#上次金額
       @cart_item.quantity -=1
-      @price = @cart_item.calculate.round
+      @o_price = @cart_item.origin_calculate.round#小計金額
+      @d_price = @cart_item.calculate.round
 
       @cart_item.save!
       
 
-      render :json => {:id =>params[:id] , :quantity => @cart_item.quantity, :price =>@price -@o_price,
-                    :o_price => @price}
+      render :json => {:id =>params[:id] , :quantity => @cart_item.quantity, :price => @l_price,
+                    :v_price => @o_price - @d_price, :o_price => @o_price, :d_price => @d_price}
     else
       render :json => {:id =>params[:id] , :quantity => 0 ,:price => 0}
     end
