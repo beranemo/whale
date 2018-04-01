bd8378fde7d9841c037c1811a0211e4e01236697
spy <sp831031@gmail.com>
2018-04-01 09:58:50 +0800

    套用bootstrap-modal gem,在成立訂單頁面可以直接設定會員

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require bootstrap-modal
+//= require bootstrap-modalmanager
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
