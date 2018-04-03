284ded12380656ac4168374c8dd363fe739ff2a7
spy <sp831031@gmail.com>
2018-04-03 15:16:02 +0800

    在所有會員資料頁面新增CSV下載功能

--- app/models/member.rb
+++ app/models/member.rb
@@ -32,4 +32,18 @@ class Member < ApplicationRecord
   belongs_to :hair_type, foreign_key: "hair_code", primary_key: "code"
   belongs_to :member_type, foreign_key: "member_code", primary_key: "code", optional: true
   has_many :orders
+
+  def self.to_csv
+    zh_attr = %w{姓名 生日 性別 email 電話 傳真 會員群組 郵遞區號 居住縣市 居住地址 紅利點數  膚質 髮質}
+    attributes = %w{name birthday gender email phone fax member_code zip county address bonus }
+    CSV.generate(headers: true) do |csv|
+      csv << zh_attr
+
+      all.each do |member|
+        data = member.attributes.values_at(*attributes)
+        data << member.skin_type.value << member.hair_type.value
+        csv << data
+      end
+    end
+  end
 end
