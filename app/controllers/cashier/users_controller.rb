d8e8777c03e93df2f9bd71d0eb4e6d21529d8ade
beranemo <beranemo@gmail.com>
2018-04-04 13:15:34 +0800

    管理員可以新增使用者帳號

--- app/controllers/cashier/users_controller.rb
+++ app/controllers/cashier/users_controller.rb
@@ -3,6 +3,23 @@ class Cashier::UsersController < ApplicationController
     @users = User.group_by_cashier
   end
   
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(users_params)
+    @user.role = "cashier"
+    @user.password = "000000"
+    if @user.save
+      flash[:notice] = "成功新增門市人員帳號"
+      redirect_to cashier_users_path
+    else
+      flash.now[:alert] = @user.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+  
   def to_resigned
     @user = User.find(params[:id])
     @user.to_resigned
@@ -16,4 +33,11 @@ class Cashier::UsersController < ApplicationController
     
     redirect_to cashier_users_path
   end  
+  
+  private
+  def users_params
+    params.require(:user).permit(
+      :email
+      )
+  end
 end
