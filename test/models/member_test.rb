a374d146c31bcdb4fe23cb8153705ebd0ec79a10
beranemo <beranemo@gmail.com>
2018-04-04 11:03:22 +0800

    修正重構後假資料產生之錯誤

--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -2,29 +2,31 @@
 #
 # Table name: members
 #
-#  id           :integer          not null, primary key
-#  name         :string           default(""), not null
-#  birthday     :date             not null
-#  gender       :string           default(""), not null
-#  phone        :string           default(""), not null
-#  skin         :string
-#  hair         :string
-#  avatar       :string
-#  remark       :string
-#  member_code  :string
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
-#  email        :string           default(""), not null
-#  fax          :string
-#  password     :string
-#  group        :string
-#  zip          :string
-#  county       :string
-#  address      :string
-#  bonus        :integer          default(0)
-#  skin_type_id :integer
-#  hair_id      :integer
-#  hair_code    :string
+#  id             :integer          not null, primary key
+#  name           :string           default(""), not null
+#  birthday       :date             not null
+#  gender         :string           default(""), not null
+#  phone          :string           default(""), not null
+#  skin           :string
+#  hair           :string
+#  avatar         :string
+#  remark         :string
+#  member_code    :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  email          :string           default(""), not null
+#  fax            :string
+#  password       :string
+#  group          :string
+#  zip            :string
+#  county         :string
+#  address        :string
+#  bonus          :integer          default(0)
+#  skin_type_id   :integer
+#  hair_id        :integer
+#  hair_code      :string
+#  hair_type_id   :integer
+#  member_type_id :integer
 #
 
 #  id           :integer          not null, primary key
