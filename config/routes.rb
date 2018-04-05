3d7d0b81600aa85588e4ec3a2328f4632bc9dd4d
beranemo <beranemo@gmail.com>
2018-04-05 20:22:39 +0800

    修復會員資料需門市人員登入才可查詢的到

--- config/routes.rb
+++ config/routes.rb
@@ -119,5 +119,5 @@ Rails.application.routes.draw do
   
   resources :users, only: [:edit, :update]
   
-  resources :members, only: [:index, :new, :create, :show]
+  resources :members, only: [:new, :create, :show]
 end
